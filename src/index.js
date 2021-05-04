63955d20e76e7642608dd0f98767cec018d458ad
MikeyCouture <michelcouture42@gmail.com>
2021-05-04 13:48:15 -0400

    setting up first action and reducer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import registerServiceWorker from "./registerServiceWorker";
 import firebase from "./firebase";
+import rootReducer from "./reducers";
+import { setUser } from "./actions";
 
 import "semantic-ui-css/semantic.min.css";
 
@@ -16,17 +18,17 @@ import {
 } from "react-router-dom";
 
 import { createStore } from "redux";
-import { Provider } from "react-redux";
+import { Provider, connect } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(() => {}, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         // console.log(user);
-
+        this.props.setUser(user);
         this.props.history.push("/");
       }
     });
@@ -41,7 +43,12 @@ class Root extends React.Component {
     );
   }
 }
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(
+  connect(
+    null,
+    { setUser }
+  )(Root)
+);
 
 ReactDOM.render(
   <Provider store={store}>
