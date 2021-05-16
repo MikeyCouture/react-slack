ee99c2769ab16f4c4790e0a20061895d05bc67b7
MikeyCouture <michelcouture42@gmail.com>
2021-05-16 19:51:05 -0400

    making sure input clears after buttom submit

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -56,12 +56,13 @@ class MessageForm extends React.Component {
   };
 
   render() {
-    const { errors } = this.state;
+    const { errors, message, loading } = this.state;
     return (
       <Segment className="message__form">
         <Input
           onChange={this.handleChange}
           fluid
+          value={message}
           name="message"
           style={{ marginBottom: "0.7em" }}
           label={<Button icon={"add"} />}
@@ -76,6 +77,7 @@ class MessageForm extends React.Component {
         <Button.Group icon widths="2">
           <Button
             onClick={this.sendMessage}
+            disabled={loading}
             color="orange"
             content="Add Reply"
             labelPosition="left"
